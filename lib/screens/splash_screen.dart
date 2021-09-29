a1642e42a9a17c23d541face993dd1583302422e
Amira-Ezz-149 <miraezz14@gmail.com>
2021-09-29 17:18:21 +0200

    constants_file

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:my_sunshine/constants.dart';
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key key}) : super(key: key);
 
@@ -8,37 +8,42 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
+          backgroundColor: Colors.white,
           body: Column(
-           // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image(
-            image: AssetImage('assets/images/home.jpg'),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            'You Are My Sunshine',
-            style: TextStyle(fontSize: 30.0,
-            color: Color(0xff0981BC),
-            fontFamily: 'Lobster'
-            ),
-          ),
-          SizedBox(height: 20.0,),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(primary: Color(0xff0981BC),),
-            onPressed: () {},
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical:15.0,
-              horizontal: 10.0),
-              child: Text('set location', style: TextStyle(fontSize: 20.0,            fontFamily: 'Lobster'
-              ),),
-            ),
-
-          ),
-        ],
-      )),
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('assets/images/home.jpg'),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'You Are My Sunshine',
+                style: TextStyle(
+                    fontSize: 30.0,
+                    color: basicColor,
+                    fontFamily: 'Lobster'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: basicColor,
+                ),
+                onPressed: () {},
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 10.0),
+                  child: Text(
+                    'set location',
+                    style: TextStyle(fontSize: 20.0, fontFamily: 'Lobster'),
+                  ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
