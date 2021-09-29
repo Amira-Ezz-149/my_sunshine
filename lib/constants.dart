7e2a594d9ca353953fd8a650fc771dee737b8a70
Monasobhy-debug <nona.lover2045@gmail.com>
2021-09-29 20:36:02 +0200

    add bottom Navigation bar

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color basicColor = Color(0xff0981BC);
---
+++ lib/screens/sunshine_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SunshineScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: Container()));
+  }
+}
