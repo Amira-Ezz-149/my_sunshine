dce6c1207e2d857f7ac6d15ba2109c652a56ad09
hager-shiref <hagershreif5@gmail.com>
2021-09-30 20:24:41 +0200

    location screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:my_sunshine/model/screens_data.dart';
-import 'package:my_sunshine/screens/app_navigation.dart';
 import 'package:my_sunshine/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
 
